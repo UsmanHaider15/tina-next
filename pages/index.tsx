65d9cd2da1f35e1a6fe3833da3c02f007c488955
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2020-12-23 11:35:35 +0500

    pushing changed to github

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,18 @@ import Head from 'next/head'
  import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
  import { GetStaticProps } from 'next'
 
-    
-export default function Home({ file }) {
-const data = file.data
+ import { usePlugin } from 'tinacms'
+ import { useGithubJsonForm, useGithubToolbarPlugins } from 'react-tinacms-github'
 
+export default function Home({ file }) {
+  const formOptions = {
+      label: 'Home Page',
+      fields: [{ name: 'title', component: 'text' }],
+    }
+      // Registers a JSON Tina Form
+  const [data, form] = useGithubJsonForm(file, formOptions)
+  usePlugin(form)
+  useGithubToolbarPlugins()
   return (
     <div className="container">
       <Head>
@@ -25,11 +33,11 @@ const data = file.data
 +          {data.title}
         </h1>
 
-        //...
+        
 
       </main>
 
-      //...
+      
 
     </div>
   )
